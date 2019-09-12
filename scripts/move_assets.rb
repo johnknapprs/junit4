e4fbd5fd11a1b723bbaf7b765c09762cccee85d3
John Knapp <knappj2@gmail.com>
2019-09-12 05:28:23 -0400

    Reverting to base

--- scripts/move_assets.rb
+++ scripts/move_assets.rb
@@ -2,9 +2,19 @@
 
 require 'fileutils'
 
-project_root = File.expand_path('.')
+project_root     = File.expand_path('.')
+jenkinsfile_path = File.join(project_root, 'Jenkinsfile')
+settings_path    = File.join(project_root, 'settings.xml')
+
 update_files = case ARGV.first.to_s.strip
                when /base/
+                 [jenkinsfile_path, settings_path].each do |file|
+                   if File.exist?(file)
+                     puts "Removing #{file}"
+                     FileUtils.rm_f(file)
+                   end
+                 end
+
                  Dir['scripts/demo_base/*'].map { |f| File.expand_path(f) }
                when /build/
                  Dir['scripts/demo_build/*'].map { |f| File.expand_path(f) }
@@ -16,7 +26,7 @@ update_files = case ARGV.first.to_s.strip
                  raise 'Unable to find matching configurations'
                end
 
-puts 'Updating Jenkinsfile'
+puts 'Updating files'
 
 update_files.each do |file|
   FileUtils.cp(file, project_root)
